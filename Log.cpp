81d4eb1e393044e33229f79101d873373d1df844
Shu-Ye Lin <67604195+shuyelin06@users.noreply.github.com>
2021-09-23 08:41:14 -0400

    Create Log.cpp

---
+++ Log.cpp
@@ -0,0 +1,13 @@
+// From the Cherno guide, is a class that allows us to maintain logs in the console.
+/*
+Log classes are really important, as it allows us to see what's going on in our code.
+They can range from simple to highly sophisticated
+*/
+
+class Log{
+    
+};
+
+int main(){
+    Log log;
+}
